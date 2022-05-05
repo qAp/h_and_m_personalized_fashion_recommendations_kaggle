f5b0fb3a4945d911399a3bba6ec8e7ae16f327b2
Jack Yu <jackchungchiehyu@gmail.com>
2022-05-05 23:33:46 +0800

    Create test set in data module setup
    This reverts commit 453cbb789bd695bc006316ddb33754107d387363.

--- fashion/data/hm.py
+++ fashion/data/hm.py
@@ -241,6 +241,14 @@ class HM(pl.LightningDataModule):
                                   num_article_ids=len(le_article.classes_),
                                   week_hist_max=self.week_hist_max)
 
+        print('Creating test set...')
+        df = pd.read_parquet(meta_data_path)
+        hm_df = create_test_dataset(df, self.week_hist_max)
+        self.test_ds = HMDataset(hm_df,
+                                 self.seq_len,
+                                 num_article_ids=len(le_article.classes_),
+                                 week_hist_max=self.week_hist_max,
+                                 is_test=True)
         del hm_df
         gc.collect()
 
@@ -268,6 +276,16 @@ class HM(pl.LightningDataModule):
                 num_workers=self.num_workers,
                 pin_memory=self.on_gpu
             )
+
+    def test_dataloader(self):
+        return DataLoader(
+            self.test_ds,
+            batch_size=self.batch_size,
+            shuffle=False,
+            drop_last=False,
+            num_workers=self.num_workers,
+            pin_memory=self.on_gpu
+        )
 
 
 def prepare_data():
